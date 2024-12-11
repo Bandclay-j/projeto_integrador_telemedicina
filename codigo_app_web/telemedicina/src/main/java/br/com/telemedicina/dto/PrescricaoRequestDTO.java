4b906aae86c18e161c2913df5ba7734c1529e4eb
LukasTech12Pro <github.lukasdelevops217.negativing@aleeas.com>
2024-12-11 08:51:18 -0300

    Desfazendo algumas coisas em PrescricaoService, PrescricaoRequestDTO e PrescricaoResponseDTO

--- codigo_app_web/telemedicina/src/main/java/br/com/telemedicina/dto/PrescricaoRequestDTO.java
+++ codigo_app_web/telemedicina/src/main/java/br/com/telemedicina/dto/PrescricaoRequestDTO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package br.com.telemedicina.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -27,33 +26,3 @@ public class PrescricaoRequestDTO {
 
     private LocalDate dataPrescricao;
 }
-=======
-package br.com.telemedicina.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class PrescricaoRequestDTO {
-    private String medicamento;
-
-    private String observacao;
-
-    @NotNull(message = "O ID do paciente é obrigatório.")
-    @JsonProperty("pacienteId")
-    private Integer pacienteId;
-
-    @NotNull(message = "O ID do médico é obrigatório.")
-    @JsonProperty("medicoId")
-    private Integer medicoId;
-
-    private LocalDate dataPrescricao;
-}
->>>>>>> 5d8188aa83fcb2a81c67f3863704ca10020d18a0
--- codigo_app_web/telemedicina/src/main/java/br/com/telemedicina/dto/PrescricaoResponseDTO.java
+++ codigo_app_web/telemedicina/src/main/java/br/com/telemedicina/dto/PrescricaoResponseDTO.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package br.com.telemedicina.dto;
 
 import lombok.AllArgsConstructor;
@@ -20,26 +19,3 @@ public class PrescricaoResponseDTO {
     private String medicoNome;
     private LocalDate dataPrescricao;
 }
-=======
-package br.com.telemedicina.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class PrescricaoResponseDTO {
-    private Integer id;
-    private String medicamento;
-    private String observacao;
-    private Integer pacienteId;
-    private String pacienteNome;
-    private Integer medicoId;
-    private String medicoNome;
-    private LocalDate dataPrescricao;
-}
->>>>>>> 5d8188aa83fcb2a81c67f3863704ca10020d18a0
